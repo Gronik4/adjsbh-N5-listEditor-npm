23b17c92336d7c237bffdd28aa454166bc4da8c5
Gronik4 <golegni@yandex.ru>
2022-02-20 11:54:03 +0300

    Попытка 2

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,15 +1,28 @@
-import { Puppeteer } from "puppeteer";
+import { puppeteer } from "puppeteer";
+import { fork } from 'child_process';
 
-jest.setTimeout(30000);
+jest.setTimeout(90000);
 describe('test Popap form', () =>{
   let browser = null;
   let page = null;
+  let server = null;
   const baseUrl = 'http://localhost:9000';
+  
   beforeAll(async () => {
-    browser = await Puppeteer.launch({
-      headless: false, // show
-      slowMo: 100,
-      devtools: true, // show devTools
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on('error', reject);
+      server.on('message', (message) => {
+        if (message === 'ok') {
+          resolve();
+        }
+      });
+    });
+
+    browser = await puppeteer.launch({
+      // headless: false, // show
+      // slowMo: 100,
+      // devtools: true, // show devTools
     });
     page = await browser.newPage();
   });
